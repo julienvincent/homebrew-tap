08b28b0ba5edef41ff4f618cabccd5f9c0a7b268
Homebrew Tap <ci@julienvincent.io>
2026-01-07 01:22:17 +0000

    Update pruner to v1.0.0-alpha.4

--- Formula/pruner.rb
+++ Formula/pruner.rb
@@ -2,27 +2,27 @@ class Pruner < Formula
   desc "Code formatter orchestrator built with Treesitter"
   homepage "https://github.com/julienvincent/pruner"
   license "MIT"
-  version "1.0.0-alpha.3"
+  version "1.0.0-alpha.4"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/julienvincent/pruner/releases/download/v1.0.0-alpha.3/pruner-macos-amd64"
-      sha256 "712d1b9e33ed077b85b6cfb0ea9a099f4c17893e88251593163e5b5dc8efaa26"
+      url "https://github.com/julienvincent/pruner/releases/download/v1.0.0-alpha.4/pruner-macos-amd64"
+      sha256 "fbd54da10808f78a69dd257ed594c1e24d9e8d1692a1b0be24805566dafd18d8"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/julienvincent/pruner/releases/download/v1.0.0-alpha.3/pruner-macos-arm64"
-      sha256 "712d1b9e33ed077b85b6cfb0ea9a099f4c17893e88251593163e5b5dc8efaa26"
+      url "https://github.com/julienvincent/pruner/releases/download/v1.0.0-alpha.4/pruner-macos-arm64"
+      sha256 "fbd54da10808f78a69dd257ed594c1e24d9e8d1692a1b0be24805566dafd18d8"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/julienvincent/pruner/releases/download/v1.0.0-alpha.3/pruner-linux-amd64"
-      sha256 "00875bfaa75cda44fa3fd2982b83f3ccbce2f6696263882fc4d8b3f20d5f0d0e"
+      url "https://github.com/julienvincent/pruner/releases/download/v1.0.0-alpha.4/pruner-linux-amd64"
+      sha256 "0ef14b2078d00cf060203ba79477c829abc18018b7b665f2199c5a47a32e2d2b"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/julienvincent/pruner/releases/download/v1.0.0-alpha.3/pruner-linux-arm64"
-      sha256 "044b4318f639867a15c1328ccf98da0c4af3c91205f1c75aaa2f16325f100698"
+      url "https://github.com/julienvincent/pruner/releases/download/v1.0.0-alpha.4/pruner-linux-arm64"
+      sha256 "5ce764411e5517ad60c46ed66a6fcee722a1b981740a772adccbd5cdf6ad91c7"
     end
   end
 
